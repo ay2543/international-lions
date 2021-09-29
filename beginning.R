476da8fce286160b58c8fb8fd1ea7f74d73b59ac
ay2543 <91579352+ay2543@users.noreply.github.com>
2021-09-28 22:06:18 -0400

    Add files via upload

---
+++ beginning.R
@@ -0,0 +1,62 @@
+
+# Clear environment so it's cleaner
+rm(list = ls())
+
+# Set working directory; when you call files they will call them from this folder
+## CHANGE ACCORDINGLY
+setwd("~/OneDrive - cumc.columbia.edu/Columbia/non-academic/2021 humana-mays case comp/files")
+
+
+# Load packages (If it's your first time ever seeing these packages, 
+  # use install.packages() first,
+  # then library() )
+
+library(readr)
+library(tidyverse)
+
+
+# Read filtered data
+data <- read_csv("filtered_data.csv")  # Should take ~15 s
+
+# Quickly check column names to be sure
+colnames(data) # 22 variables
+
+
+# [1] "X1"                                "cons_chmi"                        
+# [3] "rx_gpi2_17_pmpm_cost_t_12-9-6m_b4" "est_age"                          
+# [5] "atlas_pct_laccess_seniors15"       "atlas_percapitainc"               
+# [7] "rwjf_uninsured_adults_pct"         "atlas_type_2015_mining_no"        
+# [9] "atlas_povertyallagespct"           "covid_vaccination"                
+# [11] "atlas_pct_laccess_lowi15"          "hum_region"                       
+# [13] "atlas_hh65plusalonepct"            "sex_cd"                           
+# [15] "lang_spoken_cd"                    "atlas_pct_sbp15"                  
+# [17] "rwjf_resident_seg_black_inx"       "cons_rxadhm"                      
+# [19] "atlas_medhhinc"                    "cons_lwcm07"                      
+# [21] "atlas_low_education_2015_update"   "race_cd"   
+
+
+# The first column has no purpose: X1 is just the chronological number or entries
+
+data_final <- data[,-1]
+colnames(data_final)
+
+# [1] "cons_chmi"                         "rx_gpi2_17_pmpm_cost_t_12-9-6m_b4"
+# [3] "est_age"                           "atlas_pct_laccess_seniors15"      
+# [5] "atlas_percapitainc"                "rwjf_uninsured_adults_pct"        
+# [7] "atlas_type_2015_mining_no"         "atlas_povertyallagespct"          
+# [9] "covid_vaccination"                 "atlas_pct_laccess_lowi15"         
+# [11] "hum_region"                        "atlas_hh65plusalonepct"           
+# [13] "sex_cd"                            "lang_spoken_cd"                   
+# [15] "atlas_pct_sbp15"                   "rwjf_resident_seg_black_inx"      
+# [17] "cons_rxadhm"                       "atlas_medhhinc"                   
+# [19] "cons_lwcm07"                       "atlas_low_education_2015_update"  
+# [21] "race_cd"            
+
+
+
+
+
+
+
+
+
